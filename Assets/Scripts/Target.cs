7318c3c0c43307ceac6fa46646f93783e7262518
ltaborda <ltaborda@filo.uba.ar>
2026-02-09 08:29:37 -0300

    Se agrega efecto de pulso a la luz del target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,11 +9,20 @@ public class Target : MonoBehaviour
     public Canvas canvas;
     public RectTransform canvasRect;
     public float speed = 5f;
+    private Vector2 noiseOffset;
+    public float pulseStrength = 6f;
+    public float pulseSpeed = 1.2f;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         cam = Camera.main;
+
+        noiseOffset = new Vector3(
+            UnityEngine.Random.value * 100f,
+            UnityEngine.Random.value * 100f
+        );
+
     }
 
     void OnEnable()
@@ -40,15 +49,19 @@ public class Target : MonoBehaviour
             out localPoint
         );
         
+        float nx = Mathf.PerlinNoise(Time.time * pulseSpeed, noiseOffset.x) - 0.5f;
+        float ny = Mathf.PerlinNoise(Time.time * pulseSpeed, noiseOffset.y) - 0.5f;
+
+        Vector2 pulse = new Vector2(nx, ny) * pulseStrength;
+
         //rectTransform.anchoredPosition = localPoint;
-        rectTransform.localPosition = Vector3.Lerp(
+        var lerp = Vector2.Lerp(
             rectTransform.localPosition,
             localPoint,
             speed * Time.deltaTime
         );
+
+        rectTransform.localPosition = lerp + pulse;
         ///
-
-
     }
-
 }
